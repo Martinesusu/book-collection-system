b4f920fde7beb0a797de0f2431d3687be211ecd5
Martinesusu <tinnaphop.t@st.econ.tu.ac.th>
2024-10-19 17:11:09 +0700

    Remove unused files and folders

--- utils/db.mjs
+++
@@ -1,9 +0,0 @@
-import * as pg from "pg";
-const { Pool } = pg.default;
-
-const connectionPool = new Pool({
-  connectionString:
-    "postgresql://postgres:1412@localhost:5432/book-collection",
-});
-
-export default connectionPool;
